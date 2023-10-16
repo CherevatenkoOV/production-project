d3343ec9c1545837d8e82489b2ec1ea327bd2fec
cherevatenko.o.v <olezha.cherevatenko@gmail.com>
2023-10-16 19:28:22 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,7 +5,7 @@ import {BuildOptions} from "./types/config";
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 
 export default function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html
         }),
@@ -16,11 +16,16 @@ export default function buildPlugins({paths, isDev}: BuildOptions): webpack.Webp
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
         })
+    ];
 
-    ]
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false
+            }))
+    }
+
+    return plugins
 }
